8977d5e7158bbbea2dc1d47e85ccd08d0e6c71c3
Amy Fountain <amyfountain@SBS3624-Amy-Fountain.local>
2018-09-30 21:49:27 -0700

    Added semantic UI and more components

--- src/Colrc.js
+++ src/Colrc.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Container, Divider, Grid, Header, Image } from 'semantic-ui-react'
 
 class Colrc extends React.Component {
   render() {
     return (
-      <div className='ui three column centered grid'>
+      <div className='ui'>
         <div className='column'>
           <MainMenu />
           <Nav />
-          <Container />
+          <RootDictionary />
           <Footer />
         </div>
       </div>
@@ -21,10 +22,44 @@ class MainMenu extends React.Component {
       <div className='ui three column centered grid'>
         <div className='column'>
           <Masthead />
-          <Titlebar />
           <OptionsList />
         </div>
       </div>
+    );
+  }
+}
+
+class Masthead extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class='ui top attached header'>Coeur d'Alene Online Language Resource Center</div>
+        <div class='ui attached segment'>
+          <Titlebar />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Titlebar extends React.Component {
+  render() {
+    return (
+      <div className='ui'>
+        <h2>Root Dictionary</h2>
+      </div>
+    );
+  }
+}
+
+class OptionsList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>dictionary</li>
+        <li>history</li>
+        <li>metadata</li>
+      </ul>
     );
   }
 }
@@ -40,26 +75,58 @@ class Nav extends React.Component {
   }
 }
 
-
-class Container extends React.Component {
+class RootDictionary extends React.Component {
   render() {
     return (
-      <div className='ui three column centered grid'>
-        <div className='column'>
-        	<RootDictionary />
+      <div className='ui'>
+        <Grid columns={5}>
+          <Grid.Row>
+        	   <RootElement className='row'
+               root="√k'ʷl'1"
+               occurrence="1"
+               salish="k'ʷul'"
+               nicodemus="k'u'l"
+               english="† do, fix, make, to produce. ((stem), vt.)"
+             />
+         </Grid.Row>
+         <Grid.Row>
+           <RootElement className='row'
+             root="√k'ʷl'1"
+             occurrence="2"
+             salish="k'ʷul'+l'"
+             nicodemus="k'u'lu'l"
+             english="born, made (to be...). ((lit. H/s was born), vi.)"
+           />
+         </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+class RootElement extends React.Component {
+  render() {
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <Grid.Column>{this.props.root}</Grid.Column>
+          <Grid.Column>{this.props.occurrence}</Grid.Column>
+          <Grid.Column>{this.props.salish}</Grid.Column>
+          <Grid.Column>{this.props.nicodemus}</Grid.Column>
+          <Grid.Column>{this.props.english}</Grid.Column>
         </div>
       </div>
     );
   }
 }
 
-
 class Footer extends React.Component {
   render() {
     return (
-      <div className='ui three column centered grid'>
-        <div className='column'>
-        </div>
+      <div className='ui bottom centered'>
+        <p></p>
+        <p>coeur d'alene online language resource center copyright 2009</p>
+        project supported by the national science foundation awards BCS-1160627 and BCS-1160394
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "semantic-ui-css/semantic.css";
 import Colrc from './Colrc';
 import registerServiceWorker from './registerServiceWorker';
 

b4a056e57be8408a14645bd99a44cd0f8d918531
John Ivens <john.wagner.ivens@gmail.com>
2019-02-10 11:28:15 -0700

    Creation of mock back end, connection of mock back end to front end

--- src/RootDictionary.js
+++ src/RootDictionary.js
@@ -1,7 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ReactTable from "react-table";
+import {createResource} from "simple-cache-provider";
+import {cache} from "./cache";
+import Api from "./Api";
+import ErrorBoundary from "./ErrorBoundary";
 import "react-table/react-table.css";
+
+
+let RootsResource = createResource( async () => {
+   const response = await Api.getRoots();
+   const json = await response.json();
+
+   return json;
+});
 
 class RootDictionaryIntro extends Component {
   render() {
@@ -39,6 +51,24 @@ class RootDictionaryIntro extends Component {
 }
 
 class RootDictionary extends Component {
+  constructor() {
+    super();
+    this.state = { data: [], loading: true };
+  }
+  async componentDidMount() {
+    try {
+      const response = await fetch(`http://localhost:4000/roots`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      this.setState({ loading: false, data: json });
+    } catch (error) {
+      console.log("This is my Error: " + error);
+      this.setState({ error: error });
+    }
+  }
+
   render() {
 	const getColumnWidth = (rows, accessor, headerText) => {
 	  const maxWidth = 600
@@ -95,6 +125,10 @@ class RootDictionary extends Component {
 	  ];
 
 	  const columns = [{
+	    accessor: 'id',
+      show: false
+	  	},
+    {
 	    Header: 'Root',
 	    accessor: 'root',
 	    width: getColumnWidth(rootData, 'root', 'Root'),
@@ -105,13 +139,13 @@ class RootDictionary extends Component {
 	    accessor: 'number',
 	    width: getColumnWidth(rootData, 'number', '#'),
 	    //Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-	  	}, 
+	  	},
 	  {
 	    Header: 'Salish',
 	    accessor: 'salish',
 	    //width: getColumnWidth(rootData, 'salish', 'Salish'),
 	    //Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-	  	}, 
+	  	},
 	  {
 	    Header: 'Nicodemus',
 	    accessor: 'nicodemus',
@@ -121,20 +155,61 @@ class RootDictionary extends Component {
 	  {
 	    Header: 'English',
 	    accessor: 'english',
-	    style: { 'white-space': 'unset' } 
+	    style: { 'white-space': 'unset' }
 	    //Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
 	  	}, ];
 
+    //let roots = RootsResource.read(cache);
+/*
+    <table>
+      <thead>
+      <tr>
+          <th>Root</th>
+          <th>#</th>
+          <th>Salish</th>
+          <th>Nicodemus</th>
+          <th>English</th>
+      </tr>
+      </thead>
+      <tbody>
+      {this.state.data.map(r =>
+          <tr key={r.id}>
+              <td>{r.root}</td>
+              <td>{r.number}</td>
+              <td>{r.salish}</td>
+              <td>{r.nicodemus}</td>
+              <td>{r.english}</td>
+          </tr>
+      )}
+      </tbody>
+    </table>
+*/
+/*
+    <ErrorBoundary>
+      <React.Suspense fallback={<div>Loading</div>}>
+        <ReactTable
+          data={roots}
+          columns={columns}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+      </React.Suspense>
+    </ErrorBoundary>
+*/
+    const dataOrError = this.state.error ?
+      <div style={{ color: 'red' }}>Oops! Something went wrong!</div> :
+      <ReactTable
+        data={this.state.data}
+        loading={this.state.loading}
+        columns={columns}
+        defaultPageSize={10}
+        className="-striped -highlight"
+      />;
     return (
       <div className='ui content'>
        <RootDictionaryIntro />
         <h3>Lyon and Green-Wood's Root Dictionary</h3>
-		  <ReactTable
-		    data={rootData}
-		    columns={columns}
-	   		defaultPageSize={10}
-	   		className="-striped -highlight"
-		  />
+        {dataOrError}
       </div>
     );
   }

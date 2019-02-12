8efe15a6b3c654bd20231d9f0c1c03842df49d31
amyfou <36521868+amyfou@users.noreply.github.com>
2019-02-12 10:23:37 -0700

    added spelling and pronunciation guide to tables and json.db

--- src/SpellingPronunciation.js
+++ src/SpellingPronunciation.js
@@ -41,37 +41,51 @@ class SpellingPronunciationIntro extends Component {
 }
 
 class SpellingPronunciation extends Component {
+  constructor() {
+    super();
+    this.state = { data: [], loading: true };
+  }
+  async componentDidMount() {
+    try {
+      const response = await fetch(`http://localhost:4000/spelling`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      this.setState({ loading: false, data: json });
+    } catch (error) {
+      console.log("This is my Error: " + error);
+      this.setState({ error: error });
+    }
+  }
+
   render() {
-	const spellData=[{
-	  	"id": 1,
-	  	"nicodemus": "a",
-	  	"reichard": "a",
-	  	"salish": "a",
-	  	"english": "f<bold>a</bold>ther",
-	  	"note": " "
-	  },{
-	  	"id": 2,
-		"nicodemus": "<underline>a</underline>",
-		"reichard": "á",
-        "salish": "á",
-        "english": "no example",
-        "note": "1"
-    	}
-	  ];
 
 	const columns=[{
 			Header: 'Nicodemus',
 		    accessor: 'nicodemus',
+			filterMethod: (filter, rows) =>
+	        	matchSorter(rows, filter.value, { keys: ["nicodemus"], threshold: matchSorter.rankings.CONTAINS }),
+	            filterAll: true,
 		    Cell: row => ( <DecoratedTextSpan str={row.value} />	),
         },{
         	Header: 'Reichard',
         	accessor: 'reichard',
+		    filterMethod: (filter, rows) =>
+	        	matchSorter(rows, filter.value, { keys: ["reichard"], threshold: matchSorter.rankings.CONTAINS }),
+	            filterAll: true,
         },{
         	Header: 'Salish',
         	accessor: 'salish',
+	   	    filterMethod: (filter, rows) =>
+	        	matchSorter(rows, filter.value, { keys: ["salish"], threshold: matchSorter.rankings.CONTAINS }),
+	            filterAll: true,
         },{
         	Header: 'English',
         	accessor: 'english',
+	  	    filterMethod: (filter, rows) =>
+	        	matchSorter(rows, filter.value, { keys: ["english"], threshold: matchSorter.rankings.CONTAINS }),
+	            filterAll: true,
         	Cell: row => ( <DecoratedTextSpan str={row.value} />	),
 
         },{
@@ -80,17 +94,21 @@ class SpellingPronunciation extends Component {
         	Cell: row => ( <span className="superscript">{row.value}</span> ),
 	}];
 
+    const dataOrError = this.state.error ?
+      <div style={{ color: 'red' }}>Oops! Something went wrong!</div> :
+      <ReactTable
+        data={this.state.data}
+        loading={this.state.loading}
+        columns={columns}
+        filterable
+        defaultPageSize={20}
+        className="-striped -highlight"
+      />;
 
   return (     
 	  	<div className='ui content'>    
 	        <SpellingPronunciationIntro />
-	        <ReactTable
-			    data={spellData}
-			    columns={columns}
-		   		defaultPageSize={5}
-		   		className="-striped -highlight"
-		   		className="left"
-		  />
+			{dataOrError}
 	        <SpellFootnote />
 		</div>
 	);

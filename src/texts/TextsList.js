0a5c104a51fde71867a4a2c3046c59945115489d
amyfou <36521868+amyfou@users.noreply.github.com>
2019-04-10 09:47:28 -0700

    texts on static server!

--- src/texts/TextsList.js
+++ src/texts/TextsList.js
@@ -1,229 +1,110 @@
 import React, { Component } from 'react';
 import ReactTable from "react-table";
-import TextsTableBuilder from "./TextsTableBuilder";
+import matchSorter from 'match-sorter';
+import { withRouter } from 'react-router-dom';
 
 class TextsList extends Component {
-  render() {
-	/*function getTdProps(state, ri, ci) {
-	  console.log({ state, ri, ci });
-	  return {};
-	}*/
-var media = [
-  {
-    id: "1",
-    mediaTitle: "Reichard Texts"
-  },
-  {
-    id: "2",
-    mediaTitle: "Audio not associated with a Text"
-  },
-  {
-    id: "3",
-    mediaTitle: "Texts not associated with Reichard"
+  constructor() {
+    super();
+    this.loadTextsData = this.loadTextsData.bind(this);
+    this.state = {
+    	data: [],
+    	loading: true
+     };
   }
-];
-var collections = [
-  {
-    id: "1",
-    mediaId: "1",
-    collectionTitle: "Story of Lynx",
-    cycle: "Coyote Cycle",
-    collectionVersion: "Tom Miyal",
-    reichardNumber: "9a",
-    fieldNotesNumber: "20"
-  },
-  {
-    id: "2",
-    mediaId: "1",
-    collectionTitle: "Story of Lynx",
-    cycle: "Coyote Cycle",
-    collectionVersion: "Dorothy Nicodemus",
-    reichardNumber: "9b",
-    fieldNotesNumber: "21"
-  },
-  {
-    id: "3",
-    mediaId: "1",
-    collectionTitle: "Rabbit and Jack Rabbit",
-    cycle: "Myths not in the Coyote Cycle",
-    collectionVersion: "",
-    reichardNumber: "35",
-    fieldNotesNumber: "37"
-  },
-  {
-    id: "4",
-    mediaId: "2",
-    collectionTitle: "audio only",
-    cycle: "",
-    version: "",
-    reichardNumber: "",
-    fieldNotesNumber: ""
-  }
-];
-var textItems = [
-  {
-    id: "1",
-    collectionId: "1",
-    typedImage: "typedImage1",
-    typedPdf: "typedPdf1",
-    metaDataTyped: "",
-    handImage: "handImage1",
-    handPdf: "handPdf1",
-    metaDataHand: "",
-    pubEnglImage: "PubEnglImage",
-    pubEnglPdf: "PubEnglPdf"
-  },
-  {
-    id: "2",
-    collectionId: "2",
-    typedImage: "",
-    typedPdf: "",
-    metaDataTyped: "",
-    handImage: "",
-    handPdf: "",
-    metaDataHand: "",
-    pubEnglImage: "",
-    pubEnglPdf: ""
-  },
-  {
-    id: "3",
-    collectionId: "3",
-    typedImage: "",
-    typedPdf: "",
-    metaDataTyped: "",
-    handImage: "",
-    handPdf: "",
-    metaDataHand: "",
-    pubEnglImage: "",
-    pubEnglPdf: ""
-  }
-];
-var audioItems = [
-  {
-    id: "1",
-    collectionId: "3",
-    audioTitle: "Part 1 - in Coeur d'Alene",
-    audioLanguages: "Coeur d'Alene",
-    audioLink: "",
-    audioMetaData: ""
-  },
-  {
-    id: "2",
-    collectionId: "3",
-    audioTitle: "Part 2 - begins in Coeur d'Alene, ends in English",
-    audioLanguages: "Coeur d'Alene, English",
-    audioLink: "",
-    audioMetaData: ""
-  },
-  {
-    id: "3",
-    collectionId: "4",
-    audioTitle: "When Deer Kills Children (Song, in Coeur d'Alene)",
-    audioLanguages: "Coeur d'Alene",
-    audioLink: "",
-    audioMetaData: ""
-  }
-];
 
-const combinedData = [ 
-	{
-		id: "1",
-		mediaTitle: "Reichard Texts",
-		collectionTitle: "Cricket Rides Coyote",
-		reichardNumber: "20",
-		fieldNotesNumber: "16",
-		cycle: "Coyote Cycle",
-		collectionVersion: "",
-		typedImage: "crc_typed.png",
-		typedPdf: "crc_typed.pdf",
-		metaDataTyped: "crc_typed_meta",
-		handImage: "crc_hand.png",
-		handPdf: "crc_hand.pdf",
-		metaDataHand: "crc_hand_meta",
-		pubEnglImage: "crc_english.png",
-		pubEnglPdf: "crc_english.png",
-	},
-	{
-		id: "2",
-		mediaTitle: "Reichard Texts",
-		collectionTitle: "Chief Child of the Root (Transformer)",
-		reichardNumber: "1",
-		fieldNotesNumber: "1",
-		cycle: "Chief Child of the Root (Transformer)",
-		collectionVersion: "",
-		typedImage: "ccr_typed.png",
-		typedPdf: "ccr_typed.pdf",
-		metaDataTyped: "ccr_typed_meta",
-		handImage: "",
-		handPdf: "",
-		metaDataHand: "",
-		pubEnglImage: "ccr_english.png",
-		pubEnglPdf: "ccr_english.pdf",
-	},			
-	{
-		id: "3",
-		mediaTitle: "Reichard Texts",
-		collectionTitle: "The girls who stole dentalia (Kidnapping)",
-		reichardNumber: "24",
-		fieldNotesNumber: "24",
-		cycle: "Myths not in Coyote Cycle",
-		collectionVersion: "version",
-		typedImage: "gwsd_typed.png",
-		typedPdf: "gwsd_typed.pdf",
-		metaDataTyped: "gwsd_typed_meta",
-		handImage: "gwsd_hand.png",
-		handPdf: "gwsd_hand.pdf",
-		metaDataHand: "gwsd_hand_meta",
-		pubEnglImage: "gwsd_english.png",
-		pubEnglPdf: "gwsd_english.pdf",
-	}				
-];
-	const combinedColumns=[
-	 {
-    Header: "Texts",
-    columns: [
-	      {
-	        Header: "Title",
-	        accessor: "collectionTitle",
-	      },
-	      	      {
-	        Header: "Cycle",
-	        accessor: "cycle",
-	      },
-	    ]
-	  },
+  async componentDidMount() {
+    this.loadTextsData();
+  }
+
+  async loadTextsData() {
+    try {
+      const staticPath = 'http://localhost:3500/texts/';
+      const response = await fetch(`http://localhost:4000/texts?_embed=textfiles`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      console.log(json);
+      //Find files for each text from the static server
+      	let i = 0;
+      	while (i < json.length) {
+        //	let textJson = await this.loadTextFiles(json[i]["id"]);
+        	let j = 0;
+        	while (j < json[i]["textfiles"].length) {
+          	json[i]["textfiles"][j]["src"] = staticPath + json[i]["textfiles"][j]["subdir"] + "/" + json[i]["textfiles"][j]["src"];
+          j++;
+       }
+        //json[i]["textfiles"] = textJson;
+        json[i]["key"] = json[i]["id"];
+        i++;
+      }
+      console.log(json);
+      this.setState({ loading: false, data: json });
+    } catch (error) {
+      console.log("This is my Error: " + error);
+      this.setState({ error: error });
+    }
+  }
+	weblink(link, page) {
+		return (
+			link === '' ? page : <a href={link} target="_blank" rel="noopener noreferrer">{page}</a>
+		);
+	}
+ render() {
+	
+	const columns = [
+	    {
+		    Header: 'Title',
+		    accessor: 'title',
+	    },
+	    {
+		    Header: 'Artist',
+		    accessor: 'artist',
+	    },
+	    {
+	    	Header: 'Cycle',
+	      	accessor: 'cycle',
+	    }
 	];
 
-	  return (
+    const subcolumns = [
+
+		{
+			Header: 'Versions',
+			accessor: 'versions',
+	    	Cell: ({row, original}) => (this.weblink(original.src, original.title)),		
+	    	//Cell: ({row, original}) => (original.msType), 
+		}
+    ];
+
+    const dataOrError = this.state.error ?
+         <div style={{ color: 'red' }}>Oops! Something went wrong!</div> :
+         <ReactTable
+           data={this.state.data}
+           loading={this.state.loading}
+           columns={columns}
+           filterable
+           SubComponent={row => {
+           		return (
+           		<ReactTable
+           			data={row.original.textfiles}
+           			columns={subcolumns}
+          			defaultPageSize={row.original.textfiles.length}
+           			showPagination={false}
+           			/>
+           		);
+           	}} 
+	   	  />
+    return (
       <div className='ui content'>
-		<p></p>
-		<ReactTable
-          data={combinedData}
-          columns={combinedColumns}
-          defaultPageSize={10}
-	   	  className="-striped -highlight"
-	   	  /* defaultExpanded={{0:true, 1:true, 2:true}}*/
-	   	  SubComponent={row => {
-	   	  	return (
-	   	  		<TextsTableBuilder 
-	   	  			reichardNumber={row.original.reichardNumber}
-	   	  			fieldNotesNumber={row.original.fieldNotesNumber}
-	   	  			typedImage={row.original.typedImage}
-	   	  			typedPdf={row.original.typedPdf}
-	   	  			metaDataTyped={row.original.metaDataTyped}
-	   	  			handImage={row.original.handImage}
-	   	  			handPdf={row.original.handPdf}
-	   	  			metaDataHand={row.original.metaDataHand}
-	   	  			pubEnglImage={row.original.pubEnglImage}
-	   	  			pubEnglPdf={row.original.PubEnglPdf}
-	   	  		/>
-	   	  	)
-	   	  } }
-	   	 />
-	   </div>
-	  );
-	}
+        <h3>Texts</h3>
+        <p></p>
+        {dataOrError}
+      </div>
+    );
+  }
 }
 
+export default withRouter(TextsList);
 
-export default TextsList;
--- src/texts/TextsTableBuilder.js
+++ src/texts/TextsTableBuilder.js
@@ -7,34 +7,9 @@ class TextsTableBuilder extends Component {
 		return (  
 			<div className="Grid">
 				  <Grid>
-				  	<Grid.Row columns={4}>
-				  		<Grid.Column width={1}>
-				  		</Grid.Column>
-				  		<Grid.Column width={6}>
-				  		<strong>Field Notes</strong>
-				  		</Grid.Column>
-				  		<Grid.Column width={2}>
-				  		<strong>Translation</strong>
-				  		</Grid.Column>
-				  		<Grid.Column width={3}>
-				  		<strong>Audio</strong>
-				  		</Grid.Column>
-				  	</Grid.Row>
 				    <Grid.Row columns={7}>
-				      <Grid.Column width={1}>
-				      	{this.props.reichardNumber}, ({this.props.fieldNotesNumber})
-				      </Grid.Column>
-				      <Grid.Column width={2}>
-				        Typed: {this.props.typedImage}, {this.props.typedPdf}, {this.props.metaDataTyped} 
-				      </Grid.Column>
-				      <Grid.Column width={2}>
-				        Handwritten: {this.props.handImage}, {this.props.handPDF}, {this.props.metaDataHand}
-				      </Grid.Column>
-				      <Grid.Column width={2}>
-				        Combined view
-				      </Grid.Column>
-				      <Grid.Column width={2}>
-				        {this.props.pubEnglImage}, {this.props.pubEnglPDF}
+				      <Grid.Column width={4}>
+				      	{this.props.msType}, {this.props.fileType}
 				      </Grid.Column>
 				      <Grid.Column width={3}>
 				        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />

b2bd68f7a8e4fed928c769bf1e4c07fad2c6c6af
amyfou <36521868+amyfou@users.noreply.github.com>
2019-05-01 08:57:12 -0700

    figured out and fixed the delete error by changing db.json to remove unassociated id elements from items

--- src/spelling/SpellingPronunciationList.js
+++ src/spelling/SpellingPronunciationList.js
@@ -12,6 +12,11 @@ class SpellingPronunciationList extends Component {
     this.state = { 
     	data: [], 
     	loading: true, 
+	    nicodemusSelected: true,
+	    salishSelected: true,
+		reichardSelected: true,
+	    englishSelected: true,
+	    noteSelected: true
     };
   }
 
@@ -29,13 +34,37 @@ class SpellingPronunciationList extends Component {
     }
   }
 
+	handleNicodemusChange(value) {
+		this.setState({ nicodemusSelected: !this.state.nicodemus.Selected });
+	};
+
+	handleReichardChange(value) {
+		this.setState({ reichardSelected: !this.state.reichard.Selected });
+	};
+
+
+	handleSalishChange(value) {
+		this.setState({ salishSelected: !this.state.salish.Selected });
+	};
+
+	handleEnglishChange(value) {
+		this.setState({ englishSelected: !this.state.english.Selected });
+	};
+
+	handleNoteChange(value) {
+		this.setState({ nicodemusSelected: !this.state.nicodemus.Selected });
+	};
+
+
   render() {
+  	const { nicodemusSelected, reichardSelected, salishSelected, englishSelected, noteSelected } = this.state;
 
 	const columns=[{
 		Header: "List of Symbols",
 		columns: [{
 			Header: 'Nicodemus',
 		    accessor: 'nicodemus',
+	    	show: nicodemusSelected,
 			filterMethod: (filter, rows) =>
 	        	matchSorter(rows, filter.value, { keys: ["nicodemus"], threshold: matchSorter.rankings.CONTAINS }),
 	            filterAll: true,
@@ -43,18 +72,21 @@ class SpellingPronunciationList extends Component {
         },{
         	Header: 'Reichard',
         	accessor: 'reichard',
+	    	show: reichardSelected,
 		    filterMethod: (filter, rows) =>
 	        	matchSorter(rows, filter.value, { keys: ["reichard"], threshold: matchSorter.rankings.CONTAINS }),
 	            filterAll: true,
         },{
         	Header: 'Salish',
         	accessor: 'salish',
+	    	show: salishSelected,
 	   	    filterMethod: (filter, rows) =>
 	        	matchSorter(rows, filter.value, { keys: ["salish"], threshold: matchSorter.rankings.CONTAINS }),
 	            filterAll: true,
         },{
         	Header: 'English',
         	accessor: 'english',
+	    	show: englishSelected,
 	  	    filterMethod: (filter, rows) =>
 	        	matchSorter(rows, filter.value, { keys: ["english"], threshold: matchSorter.rankings.CONTAINS }),
 	            filterAll: true,
@@ -63,10 +95,50 @@ class SpellingPronunciationList extends Component {
         },{
         	Header: 'Note',
         	accessor: 'note',
+	    	show: noteSelected,
         	Cell: row => ( <span className="superscript">{row.value}</span> ),
 		}]
 	}];
 
+  const CheckboxSpelling = () => (
+		<div className="checkBoxMenu">
+		  <label className="checkBoxLabel">Nicodemus</label>
+		  <input
+		  	name="nicodemus"
+            type="checkbox"
+            checked={this.state.nicodemusSelected}
+            onChange={this.handleNicodemusChange.bind(this)}
+          />
+		  <label className="checkBoxLabel">Reichard</label>
+		  <input
+		  	name="reichard"
+            type="checkbox"
+            checked={this.state.reichardSelected}
+            onChange={this.handleReichardChange.bind(this)}
+          />
+          <label className="checkBoxLabel">Salish</label>
+          <input
+            name="salish"
+            type="checkbox"
+            checked={this.state.salishSelected}
+            onChange={this.handleSalishChange.bind(this)}
+          />
+          <label className="checkBoxLabel">English</label>
+          <input
+            name="english"
+            type="checkbox"
+            checked={this.state.englishSelected}
+            onChange={this.handleEnglishChange.bind(this)}
+          />
+          <label className="checkBoxLabel">Note</label>
+          <input
+            name="note"
+            type="checkbox"
+            checked={this.state.noteSelected}
+            onChange={this.handleNoteChange.bind(this)}
+          />
+		</div>
+	  );
 
     const dataOrError = this.state.error ?
       <div style={{ color: 'red' }}>Oops! Something went wrong!</div> :

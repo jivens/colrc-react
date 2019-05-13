ec116c99472438da05d112370f8bc4ec45265888
amyfou <36521868+amyfou@users.noreply.github.com>
2019-05-13 11:04:47 -0700

    integrated Formik and Yup into add stems form as a test

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -5,17 +5,17 @@ import { Link } from "react-router-dom";
 class MainMenu extends Component {
   render() {
     return (
-    <div className="ui grid" id="Banner">
-      <Grid columns={3}>
-          <Grid.Column verticalAlign='top' width={16}>
-            <Segment>
-	           	<Label as={Link} to="/" name="about" color='blue' ribbon>
-	          		COLRC
-	        	</Label>
-	        	<span>Coeur d'Alene Online Language Resource Center</span>
-        	</Segment>
-          </Grid.Column>
-      </Grid>
+    	<div className="ui grid" id="Banner">
+	      	<Grid columns={3}>
+	          	<Grid.Column verticalAlign='top' width={16}>
+	            	<Segment>
+		           		<Label as={Link} to="/" name="about" color='blue' ribbon>
+		          			COLRC
+		        		</Label>
+		        		<span>Coeur d'Alene Online Language Resource Center</span>
+	        		</Segment>
+	          	</Grid.Column>
+	      	</Grid>
       </div>
     );
   }

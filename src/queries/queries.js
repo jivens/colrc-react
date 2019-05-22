ad4f710c70c07da4ce6c7d94ea3a9b829e50379f
amyfou <36521868+amyfou@users.noreply.github.com>
2019-05-22 12:09:40 -0700

    merge conflicts, graphql for roots

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -7,10 +7,7 @@ const getUsersQuery = gql`
       first
       last
       username
-<<<<<<< HEAD
       password
-=======
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
       email
       roles
     }
@@ -23,27 +20,17 @@ const getRootsQuery = gql`
       id
       root
       number
-<<<<<<< HEAD
-      nicodemus
-      salish
-=======
       salish
       nicodemus
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
       english
     }
   }
 `;
 
 const addUserMutation = gql`
-<<<<<<< HEAD
-  mutation($first: String!, $last: String!, $username: String! $email: String!, $password: String!, $roles: String!,) {
-    addUser(first: $first, last: $last, username: $username, email: $email, password: $password, roles: $roles) {
-=======
   mutation($first: String!, $last: String!, $username: String!, $email: String!, $password: String!, $roles: String!) {
     addUser(first: $first, last: $last, username: $username, email: $email, password: $password, roles: $roles) {
       id
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
       first
       last
       username
@@ -55,14 +42,9 @@ const addUserMutation = gql`
 `;
 
 const addRootMutation = gql`
-<<<<<<< HEAD
-  mutation($root: String!, $number: Integer!, $salish: String! $nicodemus: String!, $english: String!) {
-    addRoot (root: $root, number: $number, salish: $salish, nicodemus: $nicodemus, english: $english) {
-=======
   mutation($root: String!, $number: Integer!, $salish: String!, $nicodemus: String!, $english: String!) {
     addUser(root: $root, number: $number, salish: $salish, nicodemus: $nicodemus, english: $english) {
       id
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
       root
       number
       salish
@@ -71,10 +53,7 @@ const addRootMutation = gql`
     }
   }
 `;
-<<<<<<< HEAD
-=======
 
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
 const getUserQuery = gql`
   query($id: ID) {
     user(id: $id) {
@@ -82,21 +61,13 @@ const getUserQuery = gql`
       first
       last
       username
-<<<<<<< HEAD
-      password
-      email
-=======
       email
       password
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
       roles
     }
   }
 `;
-<<<<<<< HEAD
-=======
 
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
 const getRootQuery = gql`
   query($id: ID) {
     root(id: $id) {
@@ -109,10 +80,6 @@ const getRootQuery = gql`
     }
   }
 `;
-<<<<<<< HEAD
+
 export { getUsersQuery, getRootsQuery, getUserQuery, getRootQuery, addUserMutation, addRootMutation };
 
-=======
-
-export { getUsersQuery, getRootsQuery, addUserMutation, addRootMutation, getUserQuery, getRootQuery };
->>>>>>> 950e4ac66c665da2d17dd5cd94a258dd09b42ae4
